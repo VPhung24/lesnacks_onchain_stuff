ccf3182220ef3bff23e18baee1276f8e3e7d6b18
Vivian Phung <dev+github@vivianphung.com>
2023-07-22 21:48:54 +0200

    premint NFT

---
+++ src/Tummy.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Tummy is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // Base URI required to interact with IPFS
+    string private _baseURIExtended;
+
+    constructor() ERC721("Tummy", "TUM") {
+        _setBaseURI("ipfs://");
+    }
+
+    // Sets the base URI for the collection
+    function _setBaseURI(string memory baseURI) private {
+        _baseURIExtended = baseURI;
+    }
+
+    // Overrides the default function to enable ERC721URIStorage to get the updated baseURI
+    function _baseURI() internal view override returns (string memory) {
+        return _baseURIExtended;
+    }
+
+    // Allows minting of a new NFT 
+    function mintNFT(address collector, string memory metadataURI) public onlyOwner() {
+        _tokenIds.increment(); // NFT IDs start at 1
+
+        uint256 tokenId = _tokenIds.current();
+        _safeMint(collector, tokenId);
+        _setTokenURI(tokenId, metadataURI);
+    }
+
+    // Allow updating the metadata URI of an existing NFT
+    function updateMetadataURI(uint256 tokenId, string memory metadataURI) public onlyOwner() {
+        _setTokenURI(tokenId, metadataURI);
+    }
+}
